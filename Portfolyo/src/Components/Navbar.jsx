d77d3d5eb13f1d4c2b1c6a8667090f89a312ec23
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-08-24 16:56:37 +0300

    Icons

--- Portfolyo/src/Components/Navbar.jsx
+++ Portfolyo/src/Components/Navbar.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import ReorderIcon from '@mui/icons-material/Reorder';
+import { useState, useEffect } from 'react'
 
 function Navbar() {
+    const [expandNavbar, setExpandNavbar] = useState(false)
+
+    const location = useLocation()
+
+    //Eğer benim location'ım değişirse benim navbar'ımı kapat false yap
+    useEffect(() => {
+        setExpandNavbar(false)
+    }, [location])
+
     return (
-        <div className='navbar'>
+        <div className='navbar' id={expandNavbar ? "open" : "close"}>
             <div className='toggleButton'>
-                <button></button>
-            </div>
+                <button onClick={() => { setExpandNavbar((prev) => !prev) }}><ReorderIcon /></button>
+            </div>s
             <div className='links'>
                 <Link to="/">Home</Link>
                 <Link to="/projects">Projects</Link>
