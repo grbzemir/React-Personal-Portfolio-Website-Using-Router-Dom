f4a09cfd8dfa14c1328e1354fb6e741bd5de7784
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-08-24 16:35:28 +0300

    Router, Routes And Path

--- Portfolyo/src/App.jsx
+++ Portfolyo/src/App.jsx
@@ -3,11 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Home from './Pages/Home'
+import Projects from './Pages/Projects'
+import Experience from './Pages/Experience'
 
 function App() {
 
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
